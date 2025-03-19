9134196cea1782e43b972d389794b772cb843583
JonathanJiWu <jonathanwubrave@gmail.com>
2025-03-19 16:42:02 -0400

    added and configed firebase auth and database

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'eyelevel',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'eyelevel',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
